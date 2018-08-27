291ddc5a3a9607d9c54829df76108a214b79396f
RobertBett <robertbett6@icloud.com>
2018-08-27 13:02:27 -0400

    adds success and failure messages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -9,7 +8,10 @@ class App extends Component {
     this.state = {
       username: "",
       password: "",
-      response:null
+      isAuthenticated:null,
+      response: null,
+      userRoles: [],
+      error:null
     };
   }
 
@@ -26,27 +28,53 @@ class App extends Component {
         password:this.state.password,
         username:this.state.username
     }).then((res) => {
-        this.setState({ response: res})
+      const loginData = res.data
+      this.setState({
+        response : loginData, 
+        isAuthenticated: loginData.isAuthenticated,
+        userRoles: loginData.authorities
+      })
     }).catch((err) => {
-      this.setState({ response: err.message})
+      this.setState({ error: err.message})
     });
+
+  }
+
+  renderRoles(userRoles){
+    console.log(userRoles[0])
+    const roles = userRoles.map((role)=>{
+      console.log(role)
+      return(
+        <div>
+          <p>{role}</p>
+        </div>
+      )
+    })
+    return roles
   }
 
   render() {
     return (
       <div className="login">
         <div >
-          <div>
-            <h1 className="error">{this.state.response}</h1>
+      { !this.state.isAuthenticated? <div>
+            {this.state.error? <p className='error'>Authentication Failed</p>: null}
             <h2 className="label">Login</h2>
             <div>
               <div>
-                <input onKeyPress={this.handleKeyPress} id="username"onChange={this.handleChange} value={this.state.applicationID} placeholder="UserName" className='input' name="username" type="text" />
+                <input onKeyPress={this.handleKeyPress} id="username" onChange={this.handleChange} value={this.state.applicationID} placeholder="UserName" className='input' name="username" type="text" />
                 <input onKeyPress={this.handleKeyPress} id="password" onChange={this.handleChange} value={this.state.applicationID} placeholder="Password" className='input' name="password" type="password" />
                 <button onClick={this.handleSubmit} className='input'>Login</button>
               </div>
             </div>
-          </div>
+          </div>: 
+            <div> 
+            <h1 className="success">Successful Login!</h1>
+            <h2>Welcome {this.state.response.userName} </h2>
+            <h3> User Roles</h3>
+            {this.renderRoles(this.state.userRoles)}
+            </div>
+          }
         </div>
       </div>
     );
